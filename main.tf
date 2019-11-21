3f57fff6e7af5700f346e6767eb2c22b2976de15
tecmail <57920150+tecmail@users.noreply.github.com>
2019-11-21 10:58:45 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+variable "prefix" {}
+variable "environment" {}
+variable "location" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-tfe-rg"
+  location = "${var.location}"
+
+  tags = {
+    "environment" = "${var.environment}"
+  }
+}
